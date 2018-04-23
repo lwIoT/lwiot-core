7b6af1b97ddd0f40256db2804378a89c9ad2c116
Michel Megens <dev@bietje.net>
2018-04-24 00:48:56 +0200

    project: cmake: add AVR crosscompiler target
    Add a crosscompile target.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ cmake/avr.cmake
@@ -0,0 +1,16 @@
+SET(CMAKE_SYSTEM_NAME Generic)
+SET(CMAKE_SYSTEM_VERISON 1)
+
+SET(CMAKE_C_COMPILER avr-gcc)
+SET(CMAKE_CXX_COMPILER avr-g++)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+SET(MCU "atmega2560" CACHE STRING "AVR MCU type")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mmcu=${MCU}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mmcu=${MCU}")
