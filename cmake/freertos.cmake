afaeddd2a9f85b348d3a0ea13f376ef08cf281ef
Michel Megens <dev@bietje.net>
2018-04-28 19:08:01 +0200

    project: cmake: declutter the main build files
    Move isolated functionality into its own file, instead of creating a
    single big CMakeLists.txt.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- cmake/freertos.cmake
+++ cmake/freertos.cmake
@@ -7,29 +7,18 @@ SET(LWIOT_PORT_SRCS
 SET(LWIOT_PORT_HEADERS
 ${LWIOT_PORT_DIR}/arch.h)
 
-if(FREERTOS_CONFIG)
-    include(${FREERTOS_CONFIG})
-else()
-    SET(FREERTOS_DIR "${PROJECT_SOURCE_DIR}/external/freertos" CACHE STRING "FreeRTOS directory")
-
-    SET(FREERTOS_CONFIG_DIR "${PROJECT_SOURCE_DIR}/external/freertos/config" CACHE STRING "FreeRTOS config directory")
-    SET(FREERTOS_INCLUDE_EXTRA "" CACHE STRING "Extra include libs")
-
-
-endif()
+SET(FREERTOS_DIR "${PROJECT_SOURCE_DIR}/external/freertos" CACHE STRING "FreeRTOS directory")
+SET(FREERTOS_CONFIG_DIR "${PROJECT_SOURCE_DIR}/external/freertos/config" CACHE STRING "FreeRTOS config directory")
+SET(FREERTOS_INCLUDE_EXTRA "" CACHE STRING "Extra include libs")
 
 SET(HAVE_RTOS True)
 
-if(FREERTOS_MANUAL)
-    message(STATUS ${LWIOT_PORT_INCLUDE_DIR})
-else()
-    SET(LWIOT_PORT_INCLUDE_DIRECTORIES
-            ${PROJECT_SOURCE_DIR}/source/ports/freertos
-            ${FREERTOS_CONFIG_DIR}
-            ${FREERTOS_DIR}/Source/include
-            ${FREERTOS_DIR}/Source/portable/${COMPILER}/${PORT}
-    )
-endif()
+SET(LWIOT_PORT_INCLUDE_DIRECTORIES
+        ${PROJECT_SOURCE_DIR}/source/ports/freertos
+        ${FREERTOS_CONFIG_DIR}
+        ${FREERTOS_DIR}/Source/include
+        ${FREERTOS_DIR}/Source/portable/${COMPILER}/${PORT}
+)
 
 SET(EXTRA_INCLUDE_DIRECTORIES "${EXTRA_INCLUDE_DIRECTORIES} ${FREERTOS_INCLUDE_EXTRA}")
 
