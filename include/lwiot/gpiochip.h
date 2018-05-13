9d437768deea4d0893c32c9af5bc45b42664e8e3
Michel Megens <dev@bietje.net>
2018-05-13 14:06:29 +0200

    io: gpio: add input / output methods
    Add methods to set pins as input / output.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- include/lwiot/gpiochip.h
+++ include/lwiot/gpiochip.h
@@ -4,3 +4,43 @@
  * @author Michel Megens
  * @email  dev@bietje.net
  */
+
+#pragma once
+
+#include <lwiot.h>
+
+#if CXX
+namespace lwiot
+{
+	enum PinMode {
+		INPUT,
+		INPUT_PULLUP,
+		OUTPUT,
+		OUTPUT_OPEN_DRAIN
+	};
+
+	class GpioChip {
+	public:
+		virtual ~GpioChip() = default;
+
+		virtual void mode(int pin, const PinMode& mode) = 0;
+		virtual void write(int pin, bool value) = 0;
+		virtual bool read(int pin) = 0;
+
+		virtual void open_drain() = 0;
+		virtual void od_write(int pin, bool value) = 0;
+
+		virtual void input(int pin);
+		virtual void output(int pin);
+
+
+		const unsigned int& pins() const;
+
+	protected:
+		explicit GpioChip(int pins);
+
+	private:
+		unsigned int _nr;
+	};
+}
+#endif
--- source/io/gpio/gpiochip.cpp
+++ source/io/gpio/gpiochip.cpp
@@ -4,3 +4,28 @@
  * @author Michel Megens
  * @email  dev@bietje.net
  */
+
+#include <lwiot/gpiochip.h>
+
+namespace lwiot
+{
+	GpioChip::GpioChip(int pins)
+	{
+		this->_nr = pins;
+	}
+
+	const unsigned int& GpioChip::pins() const
+	{
+		return this->_nr;
+	}
+
+	void GpioChip::input(int pin)
+	{
+		this->mode(pin, INPUT);
+	}
+
+	void GpioChip::output(int pin)
+	{
+		this->mode(pin, OUTPUT);
+	}
+}
