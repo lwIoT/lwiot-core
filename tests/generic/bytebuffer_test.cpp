b6e6e68442011af28bec6a821c929d277167ade0
Michel Megens <dev@bietje.net>
2018-05-14 20:20:28 +0200

    tests: buffer: add a byte buffer unit test
    Add a unit test to test the features of bytebuffers.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ tests/generic/bytebuffer_test.cpp
@@ -0,0 +1,34 @@
+/*
+ * ByteBuffer unit test.
+ *
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+#include <stdlib.h>
+#include <lwiot.h>
+
+#include <lwiot/bytebuffer.h>
+#include <lwiot/test.h>
+#include <lwiot/log.h>
+
+int main(int argc, char **argv)
+{
+	lwiot::ByteBuffer bf;
+
+	lwiot_init();
+	for(int i = 65; i < 75; i++)
+		bf.write((uint8_t)i);
+
+	print_dbg("Bytes in buffer: ");
+	for(auto x : bf) {
+		printf("%c ", x);
+	}
+
+	putc('\n', stdout);
+	lwiot_destroy();
+
+	wait_close();
+	return -EXIT_SUCCESS;
+}
+
