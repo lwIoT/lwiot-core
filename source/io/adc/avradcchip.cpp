f3f8a62f21c5fda556ab93911af1c74f81159fef
Michel Megens <dev@bietje.net>
2018-05-24 00:08:40 +0200

    io: adc: avr: let AvrAdcChip::read() convert to volts
    Let the `AvrAdcChip::read()' method convert to milivolts before
    returning.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- source/io/adc/avradcchip.cpp
+++ source/io/adc/avradcchip.cpp
@@ -14,8 +14,6 @@
 #include <avr/interrupt.h>
 #include <avr/common.h>
 
-static const uint8_t analog_reference = 1;
-
 #ifdef ARDUINO_MEGA
 #define ADC_PINS 16
 #elif defined(ARDUINO_UNO)
@@ -73,23 +71,24 @@ namespace lwiot
 		uint8_t high, low;
 #if defined(analogPinToChannel)
 #if defined(__AVR_ATmega32U4__)
-	if (pin >= 18) pin -= 18; // allow for channel or pin numbers
+		if (pin >= 18) pin -= 18; // allow for channel or pin numbers
 #endif
 	pin = analogPinToChannel(pin);
 #elif defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-	if (pin >= 54) pin -= 54; // allow for channel or pin numbers
+		if (pin >= 54)
+			pin -= 54;
 #elif defined(__AVR_ATmega32U4__)
-	if (pin >= 18) pin -= 18; // allow for channel or pin numbers
+		if (pin >= 18) pin -= 18; // allow for channel or pin numbers
 #elif defined(__AVR_ATmega1284__) || defined(__AVR_ATmega1284P__) || defined(__AVR_ATmega644__) || defined(__AVR_ATmega644A__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644PA__)
-	if (pin >= 24) pin -= 24; // allow for channel or pin numbers
+		if (pin >= 24) pin -= 24; // allow for channel or pin numbers
 #else
-	if (pin >= 14) pin -= 14; // allow for channel or pin numbers
+		if (pin >= 14) pin -= 14; // allow for channel or pin numbers
 #endif
 
 #if defined(ADCSRB) && defined(MUX5)
 	// the MUX5 bit of ADCSRB selects whether we're reading from channels
 	// 0 to 7 (MUX5 low) or 8 to 15 (MUX5 high).
-	ADCSRB = (ADCSRB & ~(1 << MUX5)) | (((pin >> 3) & 0x01) << MUX5);
+		ADCSRB = (ADCSRB & ~(1 << MUX5)) | (((pin >> 3) & 0x01) << MUX5);
 #endif
   
 	// set the analog reference (high two bits of ADMUX) and select the
@@ -97,9 +96,9 @@ namespace lwiot
 	// to 0 (the default).
 #if defined(ADMUX)
 #if defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
-	ADMUX = (analog_reference << 4) | (pin & 0x07);
+		ADMUX = (analog_reference << 4) | (pin & 0x07);
 #else
-	ADMUX = (analog_reference << 6) | (pin & 0x07);
+		ADMUX = _BV(REFS0) | (pin & 0x07);
 #endif
 #endif
 
@@ -107,26 +106,27 @@ namespace lwiot
 	//delay(1);
 
 #if defined(ADCSRA) && defined(ADCL)
-	// start the conversion
-	sbi(ADCSRA, ADSC);
+		// start the conversion
+		sbi(ADCSRA, ADSC);
 
-	// ADSC is cleared when the conversion finishes
-	while (bit_is_set(ADCSRA, ADSC));
+		// ADSC is cleared when the conversion finishes
+		while (bit_is_set(ADCSRA, ADSC));
 
-	// we have to read ADCL first; doing so locks both ADCL
-	// and ADCH until ADCH is read.  reading ADCL second would
-	// cause the results of each conversion to be discarded,
-	// as ADCL and ADCH would be locked when it completed.
-	low  = ADCL;
-	high = ADCH;
+		// we have to read ADCL first; doing so locks both ADCL
+		// and ADCH until ADCH is read.  reading ADCL second would
+		// cause the results of each conversion to be discarded,
+		// as ADCL and ADCH would be locked when it completed.
+		low  = ADCL;
+		high = ADCH;
 #else
-	// we dont have an ADC, return 0
-	low  = 0;
-	high = 0;
+		// we dont have an ADC, return 0
+		low  = 0;
+		high = 0;
 #endif
 
-	// combine the two bytes
-return (high << 8) | low;
+		// combine the two bytes
+		auto value = high << 8 | low;
+		return this->toVoltage(value);
 	}
 }
 
