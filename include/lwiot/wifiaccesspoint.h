a0c741e582987a6f819c1fbb05c96e2e8c852a64
Michel Megens <dev@bietje.net>
2018-06-24 01:17:37 +0200

    io: wifi: implement wifi access point mode
    Implement WiFi access point mode for the ESP32.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ include/lwiot/wifiaccesspoint.h
@@ -0,0 +1,50 @@
+/*
+ * WiFi station (mode) class definition.
+ * 
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <lwiot.h>
+
+#include <lwiot/ipaddress.h>
+#include <lwiot/log.h>
+
+
+namespace lwiot
+{
+	class WifiAccessPoint {
+	public:
+		static WifiAccessPoint& instance()
+		{
+			static WifiAccessPoint instance;
+			return instance;
+		}
+
+		WifiAccessPoint(const WifiAccessPoint&) = delete;
+		void operator =(const WifiAccessPoint&) = delete;
+
+		void begin(const String& ssid, const String& pass = "", int chan = 1, bool hidden = false, int max = 4);
+		void config(const IPAddress& local, const IPAddress& gw, const IPAddress& sn);
+		void end();
+
+		String mac() const;
+
+		const String& hostname() const;
+		void setHostname(const String& host);
+
+	private:
+		IPAddress _local, _gw, _subnet;
+		String _host;
+		bool initialised;
+
+		explicit WifiAccessPoint();
+		virtual ~WifiAccessPoint() = default;
+	};
+}
+
