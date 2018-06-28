8a83b7b66446d1be7df06405c2bb174a5484c247
Michel Megens <dev@bietje.net>
2018-06-29 00:09:36 +0200

    lib: time: add C#-style DateTime class
    Add an implementation of a C#-style DateTime class.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ include/lwiot/datetime.h
@@ -0,0 +1,79 @@
+/*
+ * C#-style DateTime implementation.
+ * 
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+#include <lwiot.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <lwiot/string.h>
+
+namespace lwiot
+{
+	typedef enum {
+		MILLISECOND,
+		SECOND,
+		MINUTE,
+		HOUR,
+		DAY,
+		MONTH,
+		YEAR
+	} time_unit_t;
+
+	class DateTime {
+	public:
+		explicit DateTime();
+		explicit DateTime(const time_t& ts);
+		DateTime(const DateTime& dt);
+
+		virtual ~DateTime() = default;
+
+		static DateTime now();
+
+		String toString() const;
+		void sync(const time_t& value);
+		void update();
+
+		const time_t& timestamp() const;
+		int year() const;
+		int month() const;
+		int day() const;
+		int dayOfWeek() const;
+		int hour() const;
+		int minute() const;
+		int second() const;
+
+		bool operator=(const time_t& time);
+		operator time_t() const;
+
+		/* Equality operators */
+		bool operator==(const DateTime& other) const;
+		bool operator!=(const DateTime& other) const;
+		bool operator<(const DateTime& other) const;
+		bool operator>(const DateTime& other) const;
+		bool operator<=(const DateTime& other) const;
+		bool operator>=(const DateTime& other) const;
+
+		DateTime operator+(const DateTime& time) const;
+		DateTime operator+(const time_t& other) const;
+		DateTime& operator+=(const DateTime& time);
+		DateTime& operator+=(const time_t& other);
+
+		DateTime operator-(const DateTime& time) const;
+		DateTime operator-(const time_t& other) const;
+		DateTime& operator-=(const DateTime& time);
+		DateTime& operator-=(const time_t& other);
+
+	private:
+		time_t stamp;
+		mutable struct tm gmt;
+
+		/* Methods */
+		void updateGmt() const;
+		void add(const time_t& other);
+		int compare(const time_t& other) const;
+	};
+}
---
+++ source/datetime.cpp
@@ -0,0 +1,219 @@
+/*
+ * C#-style DateTime implementation.
+ * 
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <lwiot.h>
+
+#include <lwiot/string.h>
+#include <lwiot/types.h>
+#include <lwiot/datetime.h>
+
+namespace lwiot
+{
+	DateTime::DateTime() : DateTime(time(NULL))
+	{
+	}
+
+	DateTime::DateTime(const DateTime& dt) : DateTime(dt.stamp)
+	{
+	}
+
+	DateTime::DateTime(const time_t& ts) : stamp(ts)
+	{
+	}
+
+	DateTime DateTime::now()
+	{
+		return DateTime(time(NULL));
+	}
+
+	const time_t& DateTime::timestamp() const
+	{
+		return this->stamp;
+	}
+
+	int DateTime::year() const
+	{
+		this->updateGmt();
+		return this->gmt.tm_year + 1900;
+	}
+
+	int DateTime::month() const
+	{
+		this->updateGmt();
+		return this->gmt.tm_mon;
+	}
+
+	int DateTime::day() const
+	{
+		this->updateGmt();
+		return this->gmt.tm_mday;
+	}
+
+	int DateTime::dayOfWeek() const
+	{
+		this->updateGmt();
+		return this->gmt.tm_wday;
+	}
+
+	int DateTime::hour() const
+	{
+		this->updateGmt();
+		return this->gmt.tm_hour;
+	}
+
+	int DateTime::minute() const
+	{
+		this->updateGmt();
+		return this->gmt.tm_min;
+	}
+
+	int DateTime::second() const
+	{
+		this->updateGmt();
+		return this->gmt.tm_sec;
+	}
+
+	String DateTime::toString() const
+	{
+		char buffer[32];
+
+		this->updateGmt();
+		memset((void*)buffer, 0, sizeof(buffer));
+		sprintf(&buffer[0], "%02i:%02i:%02i %02i-%02i-%i",
+			gmt.tm_hour, gmt.tm_min, gmt.tm_sec,
+			gmt.tm_mday, gmt.tm_mon+1, gmt.tm_year + 1900
+		);
+
+		return String(buffer);
+	}
+
+	void DateTime::sync(const time_t& value)
+	{
+		this->stamp = value;
+		this->updateGmt();
+	}
+
+	void DateTime::update()
+	{
+		this->sync(time(NULL));
+	}
+
+	void DateTime::updateGmt() const
+	{
+#ifdef WIN32
+		_gmtime64_s(&this->gmt, &this->stamp);
+#else
+		gmtime_r(&this->stamp, &this->gmt);
+#endif
+	}
+
+	DateTime DateTime::operator+(const DateTime& dt) const
+	{
+		DateTime retval(*this);
+
+		retval.stamp += dt.stamp;
+		return retval;
+	}
+
+	DateTime DateTime::operator+(const time_t& dt) const
+	{
+		DateTime retval(*this);
+
+		retval.stamp += dt;
+		return retval;
+	}
+
+	DateTime& DateTime::operator+=(const DateTime& dt)
+	{
+		this->stamp += dt.stamp;
+		return *this;
+	}
+
+	DateTime& DateTime::operator+=(const time_t& dt)
+	{
+		this->stamp += dt;
+		return *this;
+	}
+
+	DateTime DateTime::operator-(const DateTime& dt) const
+	{
+		DateTime retval(*this);
+
+		retval.stamp -= dt.stamp;
+		return retval;
+	}
+
+	DateTime DateTime::operator-(const time_t& dt) const
+	{
+		DateTime retval(*this);
+
+		retval.stamp -= dt;
+		return retval;
+	}
+
+	DateTime& DateTime::operator-=(const DateTime& dt)
+	{
+		this->stamp -= dt.stamp;
+		return *this;
+	}
+
+	DateTime& DateTime::operator-=(const time_t& dt)
+	{
+		this->stamp -= dt;
+		return *this;
+	}
+
+	/*
+	 * Comparison operators
+	 */
+
+	int DateTime::compare(const time_t& other) const
+	{
+		if(this->stamp < other)
+			return -1;
+
+		if(this->stamp > other)
+			return 1;
+
+		return 0;
+	}
+
+	bool DateTime::operator<(const DateTime& other) const
+	{
+		return this->compare(other.stamp) < 0;
+	}
+
+	bool DateTime::operator<=(const DateTime& other) const
+	{
+		return this->compare(other.stamp) <= 0;
+	}
+
+	bool DateTime::operator>(const DateTime& other) const
+	{
+		return this->compare(other.stamp) > 0;
+	}
+
+	bool DateTime::operator>=(const DateTime& other) const
+	{
+		return this->compare(other.stamp) >= 0;
+	}
+
+	bool DateTime::operator!=(const DateTime& other) const
+	{
+		return this->compare(other.stamp) != 0;
+	}
+
+	bool DateTime::operator==(const DateTime& other) const
+	{
+		return this->compare(other.stamp) == 0;
+	}
+}
