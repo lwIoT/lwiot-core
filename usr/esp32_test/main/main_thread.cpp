b1260095d3986ffba847732f3f57903aead8983f
Michel Megens <dev@bietje.net>
2018-06-29 00:10:17 +0200

    tests: chip: update on-chip unit tests
    Update the standard ESP32 on-chip unit test.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- usr/esp32_test/main/main_thread.cpp
+++ usr/esp32_test/main/main_thread.cpp
@@ -1,12 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <esp_attr.h>
+#include <esp_heap_caps.h>
 #include <lwiot.h>
 
 #include <lwiot/thread.h>
+#include <lwiot/log.h>
 #include <lwiot/string.h>
 #include <lwiot/gpiochip.h>
 #include <lwiot/gpiopin.h>
+#include <lwiot/datetime.h>
 
 class MainThread : public lwiot::Thread {
 public:
@@ -16,10 +19,18 @@ public:
 protected:
 	void run(void *arg)
 	{
-		lwiot::GpioPin out = 33;
+		lwiot::GpioPin out = 23;
+		size_t freesize;
 
 		printf("Main thread started!\n");
 		out.setOpenDrain();
+		lwiot_sleep(1000);
+
+		lwiot::DateTime dt;
+		print_dbg("Time: %s\n", dt.toString().c_str());
+		freesize = heap_caps_get_free_size(0);
+
+		print_dbg("Free heap size: %u\n", freesize);
 
 		while(true) {
 			int i = 0;
