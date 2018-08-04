c8e7bdfc7dec94fad9f5383f39fdcbd15fcff801
Michel Megens <dev@bietje.net>
2018-08-05 00:28:29 +0200

    lwiot: util: implement lambda counting utilities
    Implement the function `count()' to iterate counting (up or down) from
    `start' to `end' using lambda expressions.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ include/lwiot/count.h
@@ -0,0 +1,41 @@
+/*
+ * Lambda loop utilities.
+ * 
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+#pragma once
+
+#include <lwiot.h>
+
+template<typename Func>
+static inline int count_down(ssize_t start, ssize_t end, Func functor)
+{
+	int rv = 0;
+
+	for(ssize_t i = start; i >= end && rv >= 0; i--)
+		rv = functor(i);
+
+	return rv;
+}
+
+template<typename Func>
+static inline int count_up(ssize_t start, ssize_t end, Func functor)
+{
+	int rv = 0;
+
+	for(ssize_t i = start; i < end && rv >= 0; i++)
+		rv = functor(i);
+
+	return rv;
+}
+
+template<typename Func>
+static inline int count(ssize_t start, ssize_t end, Func functor)
+{
+	if(start < end)
+		return count_up(start, end, functor);
+	else
+		return count_down(start, end, functor);
+}
