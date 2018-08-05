c86868440674a2259763e60af48dcafb7ab9b27f
Michel Megens <dev@bietje.net>
2018-08-05 23:51:05 +0200

    test: i2c: add I2C on-chip unit test
    Add an on-chip unit test to test various I2C bus (i.e. algorithm)
    implementations.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ usr/ard-i2c-cmd-processor/ard-i2c-cmd-processor.ino
@@ -0,0 +1,111 @@
+/*
+ * I2C command processor.
+ * 
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+#include <Wire.h>
+
+#define SLAVE_ADDRESS 0x5B
+#define TIME_OUT 100
+
+#define STATE_IDLE 0
+#define STATE_RD   1
+#define STATE_WR   2
+
+#define STATE_MAX 2
+
+static char buffer[250];
+static volatile size_t buf_idx = 0;
+
+static volatile int state = 0;
+static volatile unsigned long state_set_at = 0;
+
+void setup()
+{
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
+
+  Serial.begin(115200);
+  while(!Serial);
+
+  Serial.println("Starting..");
+  memset((void*)buffer, 0, sizeof(buffer));
+}
+
+void loop()
+{
+  if(state_set_at + TIME_OUT < millis()) {
+    if(state == STATE_WR) {
+      for(int idx = 0; idx < 250; idx++) {
+        Serial.print(buffer[idx]);
+      }
+  
+      Serial.println('.');
+      memset((void*)buffer, 0, sizeof(buffer));
+    }
+
+    state = STATE_IDLE;
+    buf_idx = 0;
+  }
+
+  delay(10);
+}
+
+static void requestEvent(void)
+{
+  unsigned char x = 0xAB;
+
+  Serial.println("Read event!");
+
+  Wire.write(x++);
+  Wire.write(x++);
+  Wire.write(x);
+
+  state = STATE_IDLE;
+}
+
+static void receiveEvent(int howMany)
+{
+  switch(state) {
+  default:
+    Serial.println("Unknown state..");
+    
+    while(Wire.available()) {
+      Wire.read();
+    }
+    break;
+
+  case STATE_IDLE:
+    state = Wire.read();
+    Serial.print("State set.. ");
+    Serial.println(state);
+    buf_idx = 0;
+    state_set_at = millis();
+
+    if(state > STATE_MAX || state < 0) {
+      state = STATE_IDLE;
+
+      while(Wire.available()) {
+        Wire.read();
+      }
+      break;
+    }
+    break;
+
+  case STATE_WR:
+    while(Wire.available()) {
+      char c = Wire.read();
+
+      if(buf_idx < 250) {
+        buffer[buf_idx] = c;
+        buf_idx ++;
+      }
+    }
+    
+    break;
+  }
+}
+
---
+++ usr/esp32-i2c_test/main/main_thread.cpp
@@ -0,0 +1,195 @@
+/*
+ * ESP32 device test.
+ * 
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <esp_attr.h>
+#include <esp_heap_caps.h>
+#include <lwiot.h>
+
+#include <lwiot/thread.h>
+#include <lwiot/log.h>
+#include <lwiot/string.h>
+#include <lwiot/gpiochip.h>
+#include <lwiot/gpiopin.h>
+#include <lwiot/watchdog.h>
+#include <lwiot/datetime.h>
+#include <lwiot/i2cbus.h>
+#include <lwiot/softwarei2calgorithm.h>
+
+#include <lwiot/esp32/esp32pwm.h>
+#include <lwiot/esp32/esp32i2calgorithm.h>
+
+static int calculate_udelay_test(const uint32_t& frequency)
+{
+	double freq, tval;
+
+	if(frequency == 0U)
+		return -EINVALID;
+
+	freq = static_cast<double>(frequency);
+	tval = 1.0 / (2.0 * freq);
+	tval -= 0.000002;
+	tval *= 1e6;
+
+	return static_cast<int>(tval);
+}
+
+class MainThread : public lwiot::Thread {
+public:
+	explicit MainThread(const char *arg) : Thread("main-thread", (void*)arg)
+	{ }
+
+protected:
+	void testSingle(lwiot::I2CBus& bus)
+	{
+		lwiot::I2CMessage wr(1);
+
+		wr.setAddress(0x5B, false, false);
+		wr.write(1);
+
+		if(bus.transfer(wr)) {
+			print_dbg("Single test successfull!\n");
+		} else {
+			print_dbg("Single test failed!\n");
+		}
+
+		lwiot_sleep(500);
+	}
+
+	void testRead(lwiot::I2CBus& bus)
+	{
+		lwiot::I2CMessage wr(1), rd(3);
+		lwiot::Vector<lwiot::I2CMessage*> msgs;
+
+		wr.setAddress(0x5B, false, false);
+		wr.write(1);
+		wr.setRepeatedStart(true);
+
+		rd.setAddress(0x5B, false, true);
+
+		msgs.pushback(&wr);
+		msgs.pushback(&rd);
+
+		if(bus.transfer(msgs)) {
+			print_dbg("Read test successfull!\n");
+		} else {
+			print_dbg("Read test failed!\n");
+		}
+
+		auto& msg = msgs[1];
+		print_dbg("Read data:\n");
+		for(auto& byte : *msg) {
+			print_dbg("Read byte: %u\n", byte);
+		}
+
+		lwiot_sleep(500);
+	}
+
+	virtual void run(void *arg) override
+	{
+		size_t freesize;
+		auto algo = new lwiot::SoftwareI2CAlgorithm(23, 22, 100000U);
+		lwiot::I2CBus bus(algo);
+
+		printf("Main thread started!\n");
+		lwiot_sleep(1000);
+
+		lwiot::DateTime dt;
+		print_dbg("Time: %s\n", dt.toString().c_str());
+		freesize = heap_caps_get_free_size(0);
+		wdt.enable();
+
+		print_dbg("Free heap size: %u\n", freesize);
+		print_dbg("Software I2C udelay value: %i\n", calculate_udelay_test(100000UL));
+
+		print_dbg("Testing I2C bus..\n");
+		algo->test();
+
+		while(true) {
+			lwiot::Vector<lwiot::I2CMessage*> msgs;
+			lwiot::I2CMessage wr(1);
+			lwiot::I2CMessage rd(32);
+			lwiot::I2CMessage rd2(32);
+
+			wr.setRepeatedStart(true);
+			wr.setAddress(0x5B, false, false);
+			wr.write(2);
+			msgs.pushback(&wr);
+
+			rd.setAddress(0x5B, false, false);
+			rd.setRepeatedStart(true);
+			rd2.setAddress(0x5B, false, false);
+			rd2.setRepeatedStart(false);
+
+			for(int i = 0; i < 16; i++) {
+				rd.write('a');
+				rd.write('b');
+
+				rd2.write('c');
+				rd2.write('d');
+			}
+
+			msgs.pushback(&rd);
+			msgs.pushback(&rd);
+			msgs.pushback(&rd);
+			msgs.pushback(&rd);
+			msgs.pushback(&rd);
+			msgs.pushback(&rd);
+
+			msgs.pushback(&rd2);
+
+			print_dbg("Writing I2C msg\n");
+			if(!bus.transfer(msgs)) {
+				print_dbg("Failed to write I2C bus!\n");
+			}
+
+			lwiot_sleep(120);
+			this->testRead(bus);
+			lwiot_sleep(50);
+			this->testSingle(bus);
+
+			wdt.reset();
+			lwiot_sleep(500);
+		}
+	}
+};
+
+class TestThread : public lwiot::Thread {
+public:
+	explicit TestThread(const char *name, const char *arg) : Thread(name, (void*)arg)
+	{ }
+
+protected:
+	virtual void run(void *arg) override
+	{
+		int idx = 0;
+
+		while(idx++ < 5) {
+			print_dbg("Thread ping..\n");
+			lwiot_sleep(750);
+		}
+	}
+};
+
+static MainThread *mt;
+static TestThread *tp1, *tp2;
+static const char *arg = "Hello, World! [FROM main-thread]";
+
+extern "C" void main_start(void)
+{
+	printf("Creating main thread..");
+	mt = new MainThread(arg);
+	tp1 = new TestThread("tst-1", nullptr);
+	tp2 = new TestThread("tst-2", nullptr);
+
+	printf(" [DONE]\n");
+	mt->start();
+	tp1->start();
+	tp2->start();
+}
+
