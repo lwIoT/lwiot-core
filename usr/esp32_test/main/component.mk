c9e6184ff8fe717010d1f328f1e0398503e08bbd
Michel Megens <dev@bietje.net>
2018-11-26 17:23:46 +0100

    usr: update sdk config
    Signed-off-by: Michel Megens <dev@bietje.net>

--- usr/esp32_test/main/component.mk
+++ usr/esp32_test/main/component.mk
@@ -4,6 +4,6 @@
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
 
-BUILD_DIR = `pwd`/../../build/esp32-release/source
-COMPONENT_ADD_LDFLAGS += -L$(BUILD_DIR) -llwiot
+BUILD_DIR = `pwd`/../../build/esp32/source
+COMPONENT_ADD_LDFLAGS += -L$(BUILD_DIR) -L$(BUILD_DIR)/platform/esp32 -llwiot -llwiot-platform
 #COMPONENT_ADD_INCLUDEDIRS += $(BUILD_DIR) $(LWIOT_PATH)/include $(FREERTOS_PATH) $(PORT_PATH)
