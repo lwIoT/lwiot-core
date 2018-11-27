50bffd494eca032968cc09bd7a2d4d4b6f18f0c8
Michel Megens <dev@bietje.net>
2018-11-27 16:56:57 +0100

    esp32/esp8266: update build path references
    Update the default build path references.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- usr/esp32-display_test/main/component.mk
+++ usr/esp32-display_test/main/component.mk
@@ -4,6 +4,5 @@
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
 
-BUILD_DIR = `pwd`/../../build/esp32/source
+BUILD_DIR = `pwd`/../../build/esp32-debug/source
 COMPONENT_ADD_LDFLAGS += -L$(BUILD_DIR) -L$(BUILD_DIR)/platform/esp32 -llwiot -llwiot-platform
-
--- usr/esp32-i2c_test/main/component.mk
+++ usr/esp32-i2c_test/main/component.mk
@@ -4,7 +4,7 @@
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
 
-BUILD_DIR = `pwd`/../../build/esp32/source
+BUILD_DIR = `pwd`/../../build/esp32-debug/source
 PLATFORM_DIR = $(BUILD_DIR)/platform/esp32
 COMPONENT_ADD_LDFLAGS += -L$(BUILD_DIR) -L$(PLATFORM_DIR) -llwiot -llwiot-platform
 #COMPONENT_ADD_INCLUDEDIRS += $(BUILD_DIR) $(LWIOT_PATH)/include $(FREERTOS_PATH) $(PORT_PATH)
