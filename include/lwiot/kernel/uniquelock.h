8ce2154868363ff2a8f6e8391f7921936136ff84
Michel Megens <dev@bietje.net>
2019-02-05 23:01:33 +0100

    kernel: uniquelock: implement uniquelock
    Implement `UniqueLock<T>'. Like `ScopedLock', this class implements a
    managed lock using RAII. The template parameter is used to define the
    implementation of the actual lock it wraps. This means that `ScopedLock'
    can be defined as a function of UniqueLock:
    
    	ScopedLock = UniqueLock<Lock>
    
    In the future the definition of ScopedLock might be removed and changed
    to the using statement given above.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ include/lwiot/kernel/uniquelock.h
@@ -0,0 +1,48 @@
+/*
+ * UniqueLock definition & implementation.
+ *
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+#pragma once
+
+#include <lwiot/kernel/lock.h>
+
+namespace lwiot
+{
+	template <typename T>
+	class UniqueLock {
+	public:
+		typedef T LockType;
+
+		explicit UniqueLock(LockType & lock) : _lock(lock)
+		{
+		}
+
+		~UniqueLock()
+		{
+			if(this->_locked) {
+				this->unlock();
+			}
+		}
+
+		LockType & operator=(const LockType & lock) = delete;
+
+		void lock() const
+		{
+			this->_lock.lock();
+			this->_locked = true;
+		}
+
+		void unlock() const
+		{
+			this->_locked = false;
+			this->_lock.unlock();
+		}
+
+	private:
+		LockType & _lock;
+		mutable bool _locked;
+	};
+}
