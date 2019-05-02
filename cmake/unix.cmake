160a3fbd6909f40e1a9bc18e7ba38a37259efd8a
Michel Megens <dev@bietje.net>
2019-05-02 14:07:45 +0200

    cmake: unix: fix the UNIX build configuration
    Fix the unix build configuration by including `unix.cmake' in the root
    CMakeLists.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- cmake/unix.cmake
+++ cmake/unix.cmake
@@ -1,15 +1,5 @@
 find_path(MBEDTLS_INCLUDE_DIRS mbedtls/ssl.h)
 
-SET(PORT_INCLUDE_DIR
-	${PROJECT_SOURCE_DIR}/source/platform/ports/unix
-	${PROJECT_SOURCE_DIR}/source/platform/hosted/include
-)
-
-SET(PLATFORM_DIRECTORY ${PROJECT_SOURCE_DIR}/source/platform/unix)
-
-find_package(Threads REQUIRED)
-link_directories(${PROJECT_SOURCE_DIR}/external/freertos/lib)
-
 SET(UNIX True)
 SET(LWIOT_SYSTEM_LIBS ${LWIOT_SYSTEM_LIBS} ${CMAKE_THREAD_LIBS_INIT} mbedtls mbedx509 mbedcrypto)
 
@@ -17,5 +7,9 @@ SET(HAVE_JSON True CACHE BOOL "Build JSON library")
 SET(HAVE_NETWORKING True)
 SET(HAVE_SYNC_FETCH True)
 
-SET(PORT_C_FLAGS "-fstack-protector")
-SET(PORT_CXX_FLAGS "-fstack-protector")
+SET(PORT_C_FLAGS "-fstack-protector -Wextra -Wno-error=unused-function -Wno-error=unused-but-set-variable \
+	-Wno-error=unused-variable -Wno-error=deprecated-declarations -Wextra -Wno-unused-parameter -Wno-sign-compare \
+	 -Wno-implicit-fallthrough -fno-inline-functions")
+SET(PORT_CXX_FLAGS "-fstack-protector -Wextra -fno-rtti -Wno-error=unused-function -Wno-error=unused-but-set-variable \
+ -Wno-error=unused-variable -Wno-error=deprecated-declarations -Wextra -Wno-unused-parameter \
+ -Wno-implicit-fallthrough -Wno-sign-compare -fno-inline-functions")
