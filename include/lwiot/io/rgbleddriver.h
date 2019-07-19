50df2825a038fbab860cc99f96bb391055fb8c0a
Michel Megens <dev@bietje.net>
2019-07-19 22:23:18 +0200

    rgb: hsv: implement HSV to RGB conversion
    Implement the ability to set RGB using HSV values.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- include/lwiot/io/rgbleddriver.h
+++ include/lwiot/io/rgbleddriver.h
@@ -18,28 +18,40 @@ namespace lwiot
 {
 	class RgbLedDriver {
 	public:
-		explicit RgbLedDriver();
-		explicit RgbLedDriver(PwmChannel& r, PwmChannel& g, PwmChannel& b);
+		constexpr RgbLedDriver()
+		{
+		}
 
-		explicit RgbLedDriver(RgbLedDriver&& other) noexcept  = default;
-		explicit RgbLedDriver(const RgbLedDriver& other)  = default;
+		explicit RgbLedDriver(PwmChannel &r, PwmChannel &g, PwmChannel &b);
+		explicit RgbLedDriver(RgbLedDriver &&other) noexcept = default;
+		explicit RgbLedDriver(const RgbLedDriver &other) = default;
 
 		virtual ~RgbLedDriver() = default;
 
-		RgbLedDriver& operator=(const RgbLedDriver& rhs) = default;
-		RgbLedDriver& operator=(RgbLedDriver&& rhs) = default;
+		RgbLedDriver &operator=(const RgbLedDriver &rhs) = default;
+		RgbLedDriver &operator=(RgbLedDriver &&rhs) = default;
 
-		void setLedDrivers(const LedDriver& r, const LedDriver& g, const LedDriver& b);
-
+		void setLedDrivers(const LedDriver &r, const LedDriver &g, const LedDriver &b);
 		void set(uint8_t r, uint8_t g, uint8_t b);
 		void fade(uint8_t r, uint8_t g, uint8_t b, int ms = 30, double stepsize = 50.0);
+		void setHSV(uint8_t h, uint8_t s, uint8_t v);
+
+		void rotate(int ms = 30);
 
 	private:
 		LedDriver _red;
 		LedDriver _green;
 		LedDriver _blue;
 
+		static constexpr void clamp(double &value, double min, double max)
+		{
+			if(value < min)
+				value = min;
+			else if(value > max)
+				value = max;
+		}
+
 		/* Private methods */
-		void updateDiffValue(double& diff, double& step);
+		void updateDiffValue(double &diff, double &step);
 	};
 }
--- source/io/pwm/rgbleddriver.cpp
+++ source/io/pwm/rgbleddriver.cpp
@@ -8,22 +8,20 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <lwiot.h>
+#include <math.h>
 
 #include <lwiot/io/pwm.h>
 #include <lwiot/io/gpiopin.h>
 #include <lwiot/io/rgbleddriver.h>
 #include <lwiot/io/leddriver.h>
+#include <lwiot/system.h>
 
 #define FIX_DIF(_d_) if(_d_ < 0.0) _d_ *= -1.0
 
 namespace lwiot
 {
-	RgbLedDriver::RgbLedDriver() : _red(), _green(), _blue()
-	{
-	}
-
 	RgbLedDriver::RgbLedDriver(lwiot::PwmChannel &r, lwiot::PwmChannel &g, lwiot::PwmChannel &b) :
-		_red(r), _green(g), _blue(b)
+			_red(r), _green(g), _blue(b)
 	{
 	}
 
@@ -102,6 +100,67 @@ namespace lwiot
 			diff = 0.0;
 		} else {
 			diff += tmp;
+		}
+	}
+
+	void RgbLedDriver::setHSV(uint8_t h, uint8_t s, uint8_t v)
+	{
+		double r = 0;
+		double g = 0;
+		double b = 0;
+
+		auto i = static_cast<int>(::floor(h / 60.0));
+
+		double f = h / 60.0 - i;
+		double pv = v * (1 - s);
+		double qv = v * (1 - s * f);
+		double tv = v * (1 - s * (1 - f));
+
+		switch(i) {
+		case 0:
+			r = v;
+			g = tv;
+			b = pv;
+			break;
+		case 1:
+			r = qv;
+			g = v;
+			b = pv;
+			break;
+		case 2:
+			r = pv;
+			g = v;
+			b = tv;
+			break;
+		case 3:
+			r = pv;
+			g = qv;
+			b = v;
+			break;
+		case 4:
+			r = tv;
+			g = pv;
+			b = v;
+			break;
+		case 5:
+			r = v;
+			g = pv;
+			b = qv;
+			break;
+		}
+
+		auto red = static_cast<uint8_t> (r);
+		auto green = static_cast<uint8_t> (g);
+		auto blue = static_cast<uint8_t> (b);
+
+		this->set(red, green, blue);
+	}
+
+	void RgbLedDriver::rotate(int ms)
+	{
+		for(int idx = 0; idx < 360; idx++) {
+			this->setHSV(idx, 1, 1);
+			System::delay(ms);
 		}
 	}
 }
