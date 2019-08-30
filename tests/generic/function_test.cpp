3eea98b855ba1b0dad196c9bf486859348343e45
Michel Megens <dev@bietje.net>
2019-08-30 17:04:58 +0200

    tests: function: implement the ability to unit test functors
    Unit test the ability of the `Function' to wrap `Functor' objects.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- tests/generic/function_test.cpp
+++ tests/generic/function_test.cpp
@@ -11,7 +11,30 @@
 #include <lwiot/types.h>
 #include <lwiot/log.h>
 #include <lwiot/function.h>
+#include <lwiot/functor.h>
 #include <lwiot/test.h>
+
+class FunctorTest : public lwiot::Functor {
+public:
+	FunctorTest() = default;
+	FunctorTest(const FunctorTest& ft) = default;
+	FunctorTest( FunctorTest&& ft) = default;
+
+	FunctorTest& operator=(FunctorTest&& rhs) = default;
+	FunctorTest& operator=(const FunctorTest& rhs) = default;
+
+	void operator()() const
+	{
+		print_dbg("Const operator called!\n");
+	}
+
+protected:
+	void run() override
+	{
+		print_dbg("Functor called!");
+	}
+
+};
 
 int main(int argc, char **argv)
 {
@@ -19,6 +42,11 @@ int main(int argc, char **argv)
 
 	lwiot_init();
 	print_dbg("Testing lambda wrapping..\n");
+
+	FunctorTest functor;
+	lwiot::Function<void(void)> functor_wrapper(functor);
+
+	functor_wrapper();
 
 	fn = [&](int a) -> int {
 		return a + 1;
