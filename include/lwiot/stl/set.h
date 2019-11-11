2bdaa4d31290abb699356013a36a758f4664c854
Michel Megens <michel@michelmegens.net>
2019-11-11 23:09:55 +0100

    stl: set: implement a set iterator
    Implement a set iterator using the iterator of the underlying container
    type (`stl::SkipList').
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- include/lwiot/stl/set.h
+++ include/lwiot/stl/set.h
@@ -23,19 +23,10 @@ namespace lwiot
 
 			stl::Pair<iterator, bool> insert(const key_type& value)
 			{
-				if(this->_set.at(value))
+				if(this->contains(value))
 					return stl::Pair<iterator, bool>(this->_set.end(), false);
 
 				stl::Pair<iterator, bool> p(this->_set.insert(value, true), true);
-				return p;
-			}
-
-			stl::Pair<const_iterator, bool> insert(const key_type& value) const
-			{
-				if(this->_set.at(value))
-					return stl::Pair<const_iterator, bool>(this->_set.end(), false);
-
-				stl::Pair<const_iterator, bool> p(this->_set.insert(value, true), true);
 				return p;
 			}
 
@@ -49,6 +40,46 @@ namespace lwiot
 			stl::Pair<iterator, bool> emplace(Args&&... args)
 			{
 				return this->_set.emplace(stl::forward<Args>(args)..., true);
+			}
+
+			iterator begin()
+			{
+				return this->_set.begin();
+			}
+
+			const_iterator begin() const
+			{
+				return this->_set.begin();
+			}
+
+			iterator end()
+			{
+				return this->_set.end();
+			}
+
+			const_iterator end() const
+			{
+				return this->_set.end();
+			}
+
+			bool operator!=(const Set& other) const
+			{
+				return !(*this == other);
+			}
+
+			bool operator==(const Set& other) const
+			{
+				if(this->_set.size() != other._set.size())
+					return false;
+
+				for(auto& value : this->_set) {
+					auto v = other.contains(value);
+
+					if(!v)
+						return false;
+				}
+
+				return true;
 			}
 
 		private:
