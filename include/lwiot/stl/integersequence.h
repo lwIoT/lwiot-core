68701b856ca7d8b33e88d87536cc87cfd31cbc75
Michel Megens <michel@michelmegens.net>
2019-11-28 12:09:14 +0100

    stl: docs: add documentation
    Add documentation to the `IntegerSequence' class.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- include/lwiot/stl/integersequence.h
+++ include/lwiot/stl/integersequence.h
@@ -7,6 +7,10 @@
 
 #pragma once
 
+/**
+ * @file integersequence.h
+ */
+
 #include <stdlib.h>
 #include <stddef.h>
 
@@ -14,14 +18,28 @@ namespace lwiot
 {
 	namespace stl
 	{
+		/**
+		 * @ingroup stl
+		 * @brief Compile time representation of a sequence of integers.
+		 * @tparam T Integer type.
+		 * @tparam T... Sequence of integers.
+		 */
 		template <typename T, T... idx>
 		struct IntegerSequence {
-			typedef T ValueType;
+			typedef T ValueType; //!< Value type.
+
+			/**
+			 * @brief Get the size of the integer sequence.
+			 * @return The size of \p *this;
+			 */
 			static constexpr size_t size() noexcept
 			{
 				return sizeof...(idx);
 			}
 
+			/**
+			 * @brief Sequence type.
+			 */
 			using type = IntegerSequence<T, idx...>;
 		};
 	}
