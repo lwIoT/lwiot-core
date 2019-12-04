c9e901d518cc3cf678f517b9db34ea68e82ad811
Michel Megens <michel@michelmegens.net>
2019-12-04 17:06:28 +0100

    io: gpio: document GPIO subsystem
    Document the GPIO subsystem.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- include/lwiot/io/gpiochip.h
+++ include/lwiot/io/gpiochip.h
@@ -5,6 +5,8 @@
  * @email  dev@bietje.net
  */
 
+/// @file gpiochip.h
+
 #pragma once
 
 #include <lwiot.h>
@@ -16,45 +18,140 @@
 
 namespace lwiot
 {
+	/**
+	 * @brief GPIO IRQ edge.
+	 * @ingroup io
+	 */
 	enum IrqEdge {
-		IrqNone = 0,
-		IrqRising,
-		IrqFalling,
-		IrqRisingFalling
+		IrqNone = 0, //!< Disable the IRQ.
+		IrqRising, //!< Trigger on the rising edge.
+		IrqFalling, //!< Trigger on the falling edge.
+		IrqRisingFalling //!< Trigger on both edges.
 	};
 
+	/**
+	 * @brief GPIO peripheral abstractions.
+	 * @ingroup io
+	 */
 	class GpioChip {
 	public:
 #ifdef CONFIG_PIN_VECTOR
-		virtual ~GpioChip();
+		virtual ~GpioChip(); //!< GPIO chip destructor.
 #else
 		virtual ~GpioChip() = default;
 #endif
 
+		/**
+		 * @brief Set a pins direction.
+		 * @param pin Pin to set the direction for.
+		 * @param mode Direction to set.
+		 */
 		virtual void mode(int pin, const PinMode& mode) = 0;
+
+		/**
+		 * @brief Write to \p pin.
+		 * @param pin Pin to write to.
+		 * @param value Value to write.
+		 * @note \p pin must be OUTPUT or OUTPUT_OPEN_DRAIN.
+		 */
 		virtual void write(int pin, bool value) = 0;
+
+		/**
+		 * @brief Read from \p pin.
+		 * @param pin Pin to read from.
+		 * @return Value of \p pin.
+		 */
 		virtual bool read(int pin) const = 0;
 
+		/**
+		 * @brief Configure \p pin as open drain pin.
+		 * @param pin Pin to configure.
+		 */
 		virtual void setOpenDrain(int pin) = 0;
+
+		/**
+		 * @brief Write to an open drain pin.
+		 * @param pin Pin to write to.
+		 * @param value Value to write.
+		 * @note \p pin must be OUTPUT_OPEN_DRAIN.
+		 */
 		virtual void odWrite(int pin, bool value) = 0;
 
+		/**
+		 * @brief Attatch an IRQ handler to \p pin.
+		 * @param pin Pin to attach an IRQ handler to.
+		 * @param handler Handler to attach.
+		 * @param edge Edge to trigger on.
+		 */
 		virtual void attachIrqHandler(int pin, irq_handler_t handler, IrqEdge edge) = 0;
+
+		/**
+		 * @brief Disable an IRQ.
+		 * @param pin Pin to disable the IRQ for.
+		 */
 		virtual void detachIrqHandler(int pin) = 0;
 
+		/**
+		 * @brief Configure a pin as input.
+		 * @param pin Pin to configure.
+		 */
 		virtual void input(int pin);
+
+		/**
+		 * @brief Configure a pin as output.
+		 * @param pin Pin to configure.
+		 */
 		virtual void output(int pin);
 
+		/**
+		 * @brief Get the number of pins.
+		 * @return Number of pins.
+		 */
 		const unsigned int& pins() const;
 
+		/**
+		 * @brief Shift in a byte of data one at a time.
+		 * @param dpin Data pin.
+		 * @param cpin Clock pin.
+		 * @param lsb Bit order.
+		 * @param count Number of bits.
+		 * @param delay Delay in microseconds.
+		 * @return Value read from \p dpin.
+		 */
 		virtual uint8_t shiftIn(int dpin, int cpin, bool lsb, uint8_t count, int delay);
+
+		/**
+		 * @brief Shift a binary value into a GPIO pin.
+		 * @param dpin Data pin.
+		 * @param cpin Clock pin.
+		 * @param lsb Bit order.
+		 * @param val Value to write.
+		 * @param count Number of bits to write.
+		 * @param delay Delay in microseconds.
+		 * @return Error code
+		 */
 		virtual int shiftOut(int dpin, int cpin, bool lsb, uint8_t val, uint8_t count, int delay);
 
 #ifdef CONFIG_PIN_VECTOR
+		/**
+		 * @brief ADC chip to pin conversion
+		 * @param idx Pin number.
+		 * @return GpioPin object for \p idx.
+		 */
 		virtual GpioPin& operator[] (const size_t& idx);
+		/**
+		 * @brief ADC chip to pin conversion
+		 * @param idx Pin number.
+		 * @return GpioPin object for \p idx.
+		 */
 		virtual GpioPin& pin(size_t idx);
 #endif
 
 	protected:
+		/**
+		 * @brief Construct a new GPIO chip.
+		 * @param pins Number of pins.
+		 */
 		explicit GpioChip(int pins);
 
 	private:
--- include/lwiot/io/gpiopin.h
+++ include/lwiot/io/gpiopin.h
@@ -5,6 +5,8 @@
  * @email  dev@bietje.net
  */
 
+/// @file gpiopin.h
+
 #pragma once
 
 #include <lwiot.h>
@@ -14,52 +16,164 @@ namespace lwiot
 {
 	class GpioChip;
 
+	/**
+	 * @brief GPIO pinmode.
+	 * @ingroup io
+	 */
 	enum PinMode {
-		INPUT,
-		INPUT_PULLUP,
-		INPUT_NOPULLUP,
-		OUTPUT,
-		OUTPUT_OPEN_DRAIN
+		INPUT, //!< Input pin.
+		INPUT_PULLUP, //!< Pinput pin, pull-ups enabled.
+		INPUT_NOPULLUP, //!< Pinput pin, pull-ups disabled.
+		OUTPUT, //!< Output pin.
+		OUTPUT_OPEN_DRAIN //!< Output, configured as open drain.
 	};
 
+	/**
+	 * @brief GPIO pin abstraction.
+	 * @ingroup io
+	 */
 	class GpioPin {
 	public:
-		GpioPin(int pin);
-		explicit GpioPin();
-		explicit GpioPin(int pin, GpioChip& gpio);
-		virtual ~GpioPin();
+		/**
+		 * @brief Construct a GpioPin.
+		 * @param pin Pin number.
+		 */
+		explicit GpioPin(int pin);
 
+		/**
+		 * @brief Construct a GpioPin.
+		 */
+		explicit GpioPin();
+		/**
+		 * @brief Construct a GpioPin.
+		 * @param pin Pin number.
+		 * @param gpio GPIO peripheral.
+		 */
+		explicit GpioPin(int pin, GpioChip& gpio);
+
+		virtual ~GpioPin(); //!< GpioPin destructor.
+
+		/**
+		 * @brief Assignment operator.
+		 * @param pin Pin number.
+		 * @return A reference to \p *this.
+		 */
 		GpioPin& operator =(int pin);
+		/**
+		 * @brief Copy assignment operator.
+		 * @param pin GpioPin to copy.
+		 * @return A reference to \p *this.
+		 */
 		GpioPin& operator =(const GpioPin& pin);
 
-
+		/**
+		 * @brief Write a value.
+		 * @param value Value to write.
+		 */
 		void write(bool value);
+
+		/**
+		 * @brief Write a value.
+		 * @param value Value to write.
+		 * @return A reference to \p *this.
+		 */
 		GpioPin& operator <<(bool value);
+
+		/**
+		 * @brief Write a value.
+		 * @param value Value to write.
+		 * @see write
+		 */
 		void operator ()(bool value);
 
+		/**
+		 * @brief Read from a GpioPin.
+		 * @return The value read.
+		 */
 		bool read() const;
+
+		/**
+		 * @brief Read a value.
+		 * @param value Value read.
+		 * @return A reference to \p *this.
+		 */
 		GpioPin& operator >>(bool& value);
-		bool operator ()(void) const;
+		/**
+		 * @brief Read from a GpioPin.
+		 * @return The value read.
+		 * @see read
+		 */
+		bool operator ()() const;
+		/**
+		 * @brief Read from a GpioPin.
+		 * @return The value read.
+		 * @see read
+		 */
 		explicit operator bool() const;
+
+		/**
+		 * @brief Read from a GpioPin.
+		 * @return The value read.
+		 */
 		bool operator !() const;
 
+		/**
+		 * @brief Configure a pin as input.
+		 */
 		void input();
+		/**
+		 * @brief Configure a pin as output.
+		 */
 		void output();
+		/**
+		 * @brief Configure a pin as open drain.
+		 */
 		void setOpenDrain();
 
+		/**
+		 * @brief Configure the pin direction.
+		 * @param mode Direction to set.
+		 */
 		void mode(const PinMode& mode);
 
+		/**
+		 * @brief Shift in a byte of data one at a time.
+		 * @param clock Clock pin.
+		 * @param lsb Bit order.
+		 * @param count Number of bits.
+		 * @param delay Delay in microseconds.
+		 * @return Value read from \p dpin.
+		 */
 		uint8_t shiftIn(const GpioPin& clock, bool lsb, uint8_t count, int delay);
+
+		/**
+		 * @brief Shift a binary value into a GPIO pin.
+		 * @param clock Clock pin.
+		 * @param lsb Bit order.
+		 * @param value Value to write.
+		 * @param count Number of bits to write.
+		 * @param delay Delay in microseconds.
+		 * @return Error code
+		 */
 		int shiftOut(const GpioPin& clock, bool lsb, uint8_t value, uint8_t count, int delay);
 
+		/**
+		 * @brief Get the pin number.
+		 * @return The pin number.
+		 */
+
 		int pin() const;
+		/**
+		 * @brief Get the pin number.
+		 * @return The pin number.
+		 */
 		operator int() const;
 
-		bool operator ==(const GpioPin& pin);
-		bool operator >(const GpioPin& pin);
-		bool operator <(const GpioPin& pin);
-		bool operator >=(const GpioPin& pin);
-		bool operator <=(const GpioPin& pin);
+		bool operator ==(const GpioPin& pin); //!< Comparison operator.
+		bool operator >(const GpioPin& pin); //!< Comparison operator.
+		bool operator <(const GpioPin& pin); //!< Comparison operator.
+		bool operator >=(const GpioPin& pin); //!< Comparison operator.
+		bool operator <=(const GpioPin& pin); //!< Comparison operator.
 
 	private:
 		bool _open_drain;
