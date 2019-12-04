71cb1216754cf2ccfcda45d7f9d01884b0b2d400
Michel Megens <michel@michelmegens.net>
2019-12-04 17:10:54 +0100

    io: spi: document the SPI layer
    Document the SPI abstraction layer / driver.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- include/lwiot/io/spibus.h
+++ include/lwiot/io/spibus.h
@@ -5,6 +5,8 @@
  * @email  dev@bietje.net
  */
 
+/// @file spibus.h
+
 #pragma once
 
 #include <lwiot.h>
@@ -18,24 +20,70 @@
 #ifdef __cplusplus
 namespace lwiot
 {
+	/**
+	 * @brief SPI bus abstraction.
+	 * @ingroup io
+	 */
 	class SpiBus {
 	public:
+		/**
+		 * @brief Create a new SPI bus.
+		 * @param mosi MOSI pin.
+		 * @param miso MISO pin.
+		 * @param clk Clock pin.
+		 * @param freq Bus frequency.
+		 */
 		explicit SpiBus(int mosi, int miso, int clk, uint32_t freq = 1000000UL);
-		virtual ~SpiBus() = default;
+		virtual ~SpiBus() = default; //!< SPI bus destructor.
 
+		/**
+		 * @brief Getter for the bus frequency.
+		 * @return The bus frequency.
+		 */
 		const uint32_t& frequency() const;
+
+		/**
+		 * @brief Getter for the MOSI pin.
+		 * @return An IO pin object.
+		 */
 		const GpioPin& miso() const;
+
+		/**
+		 * @brief Getter for the MISO pin.
+		 * @return An IO pin object.
+		 */
 		const GpioPin& mosi() const;
+
+		/**
+		 * @brief Getter for the CLK pin.
+		 * @return An IO pin object.
+		 */
 		const GpioPin& clk() const;
 
+		/**
+		 * @brief Transfer a SPI message.
+		 * @param msg Message to transfer.
+		 * @return A success indicator.
+		 */
 		virtual bool transfer(SpiMessage& msg) = 0;
+
+		/**
+		 * @brief Transfer a SPI message.
+		 * @param msgs Messages to transfer.
+		 * @return A success indicator.
+		 */
 		virtual bool transfer(stl::Vector<SpiMessage>& msgs);
+
+		/**
+		 * @brief Set the bus frequency.
+		 * @param freq Frequency to set.
+		 */
 		virtual void setFrequency(uint32_t freq);
 
 	protected:
-		GpioPin _miso;
-		GpioPin _mosi;
-		GpioPin _clk;
+		GpioPin _miso; //!< MISO pin.
+		GpioPin _mosi; //!< MOSI pin.
+		GpioPin _clk; //!< Clock pin.
 
 	private:
 		uint32_t _freq;
--- include/lwiot/io/spimessage.h
+++ include/lwiot/io/spimessage.h
@@ -5,6 +5,8 @@
  * @email  dev@bietje.net
  */
 
+/// @file spimessage.h
+
 #pragma once
 
 #include <stdlib.h>
@@ -20,21 +22,76 @@
 #ifdef __cplusplus
 namespace lwiot
 {
+	/**
+	 * @brief SPI message class.
+	 * @ingroup io
+	 */
 	class SpiMessage {
 	public:
+		/**
+		 * @brief Construct a new SpiMessage object.
+		 * @param size Message size.
+		 * @param cs Chip select pin.
+		 */
 		explicit SpiMessage(const size_t& size, int cs = 0);
+
+		/**
+		 * @brief Construct a new SpiMessage object.
+		 * @param size Message size.
+		 * @param pin Chip select pin.
+		 */
 		explicit SpiMessage(const size_t& size, const GpioPin& pin);
+
+		/**
+		 * @brief SPI message destructor.
+		 */
 		virtual ~SpiMessage() = default;
 
+		/**
+		 * @brief Get the receive buffer.
+		 * @return The receive buffer.
+		 */
 		ByteBuffer& rxdata();
+
+		/**
+		 * @brief Get the transmit buffer.
+		 * @return The transmit buffer.
+		 */
 		ByteBuffer& txdata();
 
+		/**
+		 * @brief Shift data into the message.
+		 * @param byte Data to add to the transmit buffer.
+		 * @return A reference to \p *this.
+		 */
 		SpiMessage& operator <<(uint8_t byte);
+
+		/**
+		 * @brief Shift data into the message.
+		 * @param msg Data to add to the transmit buffer.
+		 * @note \p msg must contain a null terminator.
+		 * @return A reference to \p *this.
+		 */
 		SpiMessage& operator<<(const uint8_t *msg);
 
+		/**
+		 * @brief Shift data out of the receive buffer.
+		 * @param byte Buffer object.
+		 * @return A reference to \p *this.
+		 */
 		SpiMessage& operator >>(uint8_t& byte);
 
+		/**
+		 * @brief Get the message size.
+		 * @return The message size.
+		 */
 		const size_t& size() const;
+
+		/**
+		 * @brief Get the chip select pin.
+		 * @return The chip select pin.
+		 * @see GpioPin
+		 */
 		const GpioPin& cspin() const;
 
 	private:
