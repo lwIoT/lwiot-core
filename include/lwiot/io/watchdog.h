0422e9c3f974ed982490286b790cc08062a117d0
Michel Megens <michel@michelmegens.net>
2019-12-04 17:15:46 +0100

    io: wdt: document the watchdog
    Add Doxygen documentation/comments.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- include/lwiot/io/watchdog.h
+++ include/lwiot/io/watchdog.h
@@ -5,20 +5,48 @@
  * @email  dev@bietje.net
  */
 
+/// @file watchdog.h
+
 #pragma once
 
 #include <stdint.h>
 
 namespace lwiot
 {
+	/**
+	 * @brief WDT abstraction layer.
+	 * @ingroup io
+	 */
 	class Watchdog {
 	public:
+		/**
+		 * @brief Enable the watchdog.
+		 * @param tmo Timeout.
+		 * @return Success indicator.
+		 */
 		virtual bool enable(uint32_t tmo);
+
+		/**
+		 * @brief Disable the watchdog.
+		 * @return Success indicator.
+		 */
 		virtual bool disable();
+
+		/**
+		 * @brief Reset the watchdog.
+		 */
 		virtual void reset() = 0;
 
 	protected:
+		/**
+		 * @brief Construct new watchdog object.
+		 */
 		explicit Watchdog();
+
+		/**
+		 * @brief Check if the watchdog is enabled.
+		 * @return True or false based on the status of the WDT.
+		 */
 		bool enabled() const;
 
 	private:
@@ -26,4 +54,4 @@ namespace lwiot
 	};
 }
 
-extern lwiot::Watchdog& wdt;
+extern lwiot::Watchdog& wdt; //!< Global watchdog timer.
