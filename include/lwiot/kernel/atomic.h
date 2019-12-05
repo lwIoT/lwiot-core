7cb173faac3f67b7fd5d64846bd5a98c52b49c56
Michel Megens <michel@michelmegens.net>
2019-12-05 17:24:38 +0100

    kernel: atomic: document the atomic types
    Add Doxygen documentation to the atomic types defined by the lwIoT
    kernel library.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- include/lwiot/kernel/atomic.h
+++ include/lwiot/kernel/atomic.h
@@ -5,6 +5,8 @@
  * @email  dev@bietje.net
  */
 
+/// @file atomic.h
+
 #pragma once
 
 #include <stdint.h>
@@ -18,22 +20,25 @@
 
 namespace lwiot
 {
-	typedef lwiot::Atomic<uint8_t> atomic_uint8_t;
-	typedef lwiot::Atomic<uint16_t> atomic_uint16_t;
-	typedef lwiot::Atomic<uint32_t> atomic_uint32_t;
-	typedef lwiot::Atomic<uint64_t> atomic_uint64_t;
-
-	typedef lwiot::Atomic<short> atomic_short_t;
-	typedef lwiot::Atomic<int> atomic_int_t;
-	typedef lwiot::Atomic<long> atomic_long_t;
-	typedef lwiot::Atomic<long long> atomic_long_long_t;
-
+	/**
+	 * @brief Atomic boolean type.
+	 * @ingroup kernel
+	 */
 	class AtomicBool : public lwiot::detail::Atomic<uint8_t> {
 	public:
+		/**
+		 * @brief Construct a new atomic boolean type.
+		 * @param value Initial value.
+		 */
 		explicit AtomicBool(bool value = false) : detail::Atomic<uint8_t>((uint8_t)value)
 		{
 		}
 
+		/**
+		 * @brief Atomically assign a new value.
+		 * @param v Value to assign.
+		 * @return A reference to \p *this.
+		 */
 		AtomicBool& operator=(bool v) noexcept
 		{
 			this->store(uint8_t(v));
@@ -41,6 +46,11 @@ namespace lwiot
 			return *this;
 		}
 
+		/**
+		 * @brief Atomically assign a new value.
+		 * @param ab Value to assign.
+		 * @return A reference to \p *this.
+		 */
 		AtomicBool& operator=(const AtomicBool& ab)
 		{
 			bool v = ab.load() != 0;
@@ -49,6 +59,10 @@ namespace lwiot
 			return *this;
 		}
 
+		/**
+		 * @brief Atomically load the boolean value.
+		 * @return The current value of \p *this.
+		 */
 		operator bool() noexcept
 		{
 			bool value = this->load();
@@ -56,5 +70,19 @@ namespace lwiot
 		}
 	};
 
-	typedef AtomicBool atomic_bool_t;
+	/**
+	 * @addtogroup kernel
+	 * @{
+	 */
+	typedef lwiot::Atomic<uint8_t> atomic_uint8_t; //!< 8-bit atomic type.
+	typedef lwiot::Atomic<uint16_t> atomic_uint16_t; //!< 16-bit atomic type.
+	typedef lwiot::Atomic<uint32_t> atomic_uint32_t; //!< 32-bit atomic type.
+	typedef lwiot::Atomic<uint64_t> atomic_uint64_t; //!< 64-bit atomic type.
+
+	typedef lwiot::Atomic<short> atomic_short_t; //!< Short atomic type.
+	typedef lwiot::Atomic<int> atomic_int_t; //!< Integer atomic type.
+	typedef lwiot::Atomic<long> atomic_long_t; //!< Long atomic type.
+	typedef lwiot::Atomic<long long> atomic_long_long_t; //!< Long long atomic type.
+	typedef AtomicBool atomic_bool_t; //!< Atomic boolean type.
+	/** @} */
 }
