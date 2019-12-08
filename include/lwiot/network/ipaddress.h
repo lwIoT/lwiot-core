f02b91563e387a403c9634197c410e6624e44d32
Michel Megens <michel@michelmegens.net>
2019-12-08 16:38:07 +0100

    net: ip: a document the IP address wrapper
    Document the IP address wrapper using Doxygen.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- include/lwiot/network/ipaddress.h
+++ include/lwiot/network/ipaddress.h
@@ -5,6 +5,8 @@
  * @email  dev@bietje.net
  */
 
+/// @file ipaddress.h
+
 #pragma once
 
 #include <lwiot/types.h>
@@ -14,19 +16,62 @@
 
 namespace lwiot
 {
+	/**
+	 * @brief Bind address.
+	 * @ingroup net
+	 */
 	using BindAddress = bind_addr_t;
 
+	/**
+	 * @brief IP address wrapper.
+	 * @ingroup net
+	 */
 	class IPAddress {
 	public:
-		explicit IPAddress();
+		explicit IPAddress(); //!< Construct an IP address.
+
+		/**
+		 * @brief Construct an IP address.
+		 * @param first First ocet.
+		 * @param second Second ocet.
+		 * @param third Third ocet.
+		 * @param forth Forth ocet.
+		 */
 		explicit IPAddress(uint8_t first, uint8_t second, uint8_t third, uint8_t forth);
+
+		/**
+		 * @brief Construct an IP address.
+		 * @param address 32-bit address buffer.
+		 */
 		explicit IPAddress(const uint8_t *address);
+
+		/**
+		 * @brief Construct an IP address.
+		 * @param address 32-bit address.
+		 */
 		explicit IPAddress(uint32_t address);
+
+		/**
+		 * @brief Construct an IP address.
+		 * @param remote Remote address.
+		 */
 		explicit IPAddress(const remote_addr_t& remote);
 		virtual ~IPAddress() = default;
 
-		virtual String toString() const;
+		virtual String toString() const; //!< Conver the IP address to a string.
+
+		/**
+		 * @brief Convert a string to an IP address object.
+		 * @param str String to convert.
+		 * @return An IP address object.
+		 */
 		static IPAddress fromString(const String& str);
+
+		/**
+		 * @brief Convert a string to an IP address object.
+		 * @param str String to convert.
+		 * @return An IP address object.
+		 */
 		static IPAddress fromString(const char *str);
 
 		/* Operators */
@@ -41,18 +86,19 @@ namespace lwiot
 		IPAddress& operator =(uint32_t addr);
 		IPAddress& operator =(const IPAddress& addr);
 
-		void toRemoteAddress(remote_addr_t& remote) const;
+		void toRemoteAddress(remote_addr_t& remote) const; //!< Convert to a remote address.
 
-		int version() const { return this->_version; }
-		bool isIPv6() const { return this->_version == 6; }
+		int version() const { return this->_version; } //!< Get the IP version.
+		bool isIPv6() const { return this->_version == 6; } //!< Check IPv6.
 
-		static IPAddress fromBindAddress(BindAddress addr);
+		static IPAddress fromBindAddress(BindAddress addr); //!< Convert to an IP address object.
 
 	private:
 		union {
 			uint8_t bytes[16];
 			uint32_t dword[4];
 		} _address;
+
 		int _version;
 
 		const uint8_t *raw() const;
