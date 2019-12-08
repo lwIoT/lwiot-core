b9caa6b9fa806900d13a2d547c567cc4c328bc31
Michel Megens <michel@michelmegens.net>
2019-12-08 16:42:55 +0100

    net: udp: document the UDP server
    Add Doxygen documentation.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- include/lwiot/network/udpserver.h
+++ include/lwiot/network/udpserver.h
@@ -5,6 +5,8 @@
  * @email  dev@bietje.net
  */
 
+/// @file udpserver.h
+
 #pragma once
 
 #include <lwiot/network/ipaddress.h>
@@ -12,22 +14,42 @@
 
 namespace lwiot
 {
+	/**
+	 * @brief UDP server wrapper.
+	 */
 	class UdpServer {
 	public:
-		explicit UdpServer();
+		explicit UdpServer(); //!< Construct a UDP server.
+
+		/**
+		 * @brief UDP server wrapper.
+		 * @param addr Bind address.
+		 * @param port Local port.
+		 */
 		explicit UdpServer(const IPAddress& addr, uint16_t port);
-		virtual ~UdpServer() = default;
+		virtual ~UdpServer() = default; //!< Destroy a UDP server.
 
-		virtual void close() = 0;
-		virtual bool bind() = 0;
-		virtual bool bind(bind_addr_t addr, uint16_t port) = 0;
-		virtual bool bind(const IPAddress& addr, uint16_t port);
+		virtual void close() = 0; //!< Close the UDP server socket.
+		virtual bool bind() = 0; //!< Bind the UDP server.
+		virtual bool bind(bind_addr_t addr, uint16_t port) = 0; //!< Bind the UDP server.
+		virtual bool bind(const IPAddress& addr, uint16_t port); //!< Bind the UDP server.
 
+		/**
+		 * @brief Receive data from the remote.
+		 * @param buffer Input buffer.
+		 * @param length Length of \p buffer.
+		 * @return The remote client.
+		 */
 		virtual UniquePointer<UdpClient> recv(void *buffer, size_t& length) = 0;
+
+		/**
+		 * @brief Set the timeout of the server socket in seconds.
+		 * @param tmo Timeout in seconds.
+		 */
 		virtual void setTimeout(int tmo) = 0;
 
-		const IPAddress& address() const;
-		uint16_t port() const;
+		const IPAddress& address() const; //!< Get the remote address.
+		uint16_t port() const; //!< Get the remote port.
 
 	private:
 		IPAddress _bind_addr;
