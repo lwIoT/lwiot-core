0697d949a158241bff3c31af7d55a3c888d4ce75
Michel Megens <michel@michelmegens.net>
2019-12-08 16:44:10 +0100

    net: xbee: document the XBee device drivers
    Add Doxygen documentation to the XBee device driver and the asynchronous
    XBee device driver.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- include/lwiot/network/xbee/asyncxbee.h
+++ include/lwiot/network/xbee/asyncxbee.h
@@ -5,6 +5,8 @@
  * @email  dev@bietje.net
  */
 
+/// @file asyncxbee.h
+
 #pragma once
 
 #include <stdlib.h>
@@ -30,15 +32,23 @@
 
 namespace lwiot
 {
+	/**
+	 * @brief Asynchronous XBee driver.
+	 */
 	class AsyncXbee : public Thread {
 	public:
-		typedef Function<void(XBeeResponse&)> ResponseHandler;
+		typedef Function<void(XBeeResponse&)> ResponseHandler; //!< Response handler.
 
-		explicit AsyncXbee();
-		explicit AsyncXbee(XBee& xb);
-		explicit AsyncXbee(Stream& stream);
-		~AsyncXbee() override;
+		explicit AsyncXbee(); //!< Construct a new XBee driver.
+		explicit AsyncXbee(XBee& xb); //!< Construct a new XBee driver.
+		explicit AsyncXbee(Stream& stream); //!< Construct a new XBee driver.
+		~AsyncXbee() override; //!< Destroy an XBee driver.
 
+		/**
+		 * @brief Start the XBee driver.
+		 * @tparam T Handler functor type.
+		 * @param handler Handler functor.
+		 */
 		template <typename T>
 		void begin(T&& handler)
 		{
@@ -46,14 +56,45 @@ namespace lwiot
 			this->init();
 		}
 
+		/**
+		 * @brief Start the XBee driver.
+		 * @param handler Handler functor.
+		 */
 		void begin(const ResponseHandler& handler);
 
+		/**
+		 * @brief Set the response handler.
+		 * @param handler Response handler to set.
+		 */
 		void setHandler(const ResponseHandler& handler);
+
+		/**
+		 * @brief Set the underlying XBee driver.
+		 * @param xb XBee driver to set.
+		 * @see XBee
+		 */
 		void setDevice(XBee& xb);
+
+		/**
+		 * @brief Get the underlying XBee device.
+		 * @return The underlying XBee device driver.
+		 */
 		XBee& getDevice();
 
+		/**
+		 * @brief Send a request.
+		 * @param request Request to send.
+		 * @return Success indicator.
+		 */
 		bool send(XBeeRequest& request) const;
 
+		/**
+		 * @brief Send a request.
+		 * @tparam Func Callback type.
+		 * @param rq Request to send.
+		 * @param callback handler.
+		 * @return Success indicator.
+		 */
 		template <typename Func>
 		void send(XBeeRequest& rq, Func&& callback) const
 		{
@@ -61,11 +102,17 @@ namespace lwiot
 			callback(success);
 		}
 
-		bool transmit(const stl::String& data, uint16_t addr) const;
-		bool transmit(const stl::String& data, uint64_t addr) const;
-		bool transmit(ZigbeeAddress addr, const ByteBuffer& buffer) const;
-		bool transmit(ZigbeeAddress addr, const ByteBuffer& buffer, uint16_t profile, uint16_t cluster) const;
+		bool transmit(const stl::String& data, uint16_t addr) const; //!< Transmit data.
+		bool transmit(const stl::String& data, uint64_t addr) const; //!< Transmit data.
+		bool transmit(ZigbeeAddress addr, const ByteBuffer& buffer) const; //!< Transmit data.
+		bool transmit(ZigbeeAddress addr, const ByteBuffer& buffer, uint16_t profile, uint16_t cluster) const; //!< Transmit data.
 
+		/**
+		 * @brief Set a handler (callback).
+		 * @tparam Func Callback type.
+		 * @param handler Handler to set.
+		 * @see ResponseHandler
+		 */
 		template <typename Func>
 		void setHandler(Func&& handler)
 		{
@@ -73,30 +120,50 @@ namespace lwiot
 			this->_handler = stl::forward<Func>(handler);
 		}
 
-		uint64_t getHardwareAddress() const;
-		uint16_t getNetworkAddress() const;
+		uint64_t getHardwareAddress() const; //!< Get the hardware address.
+		uint16_t getNetworkAddress() const; //!< Get the network address.
 
-		void setNetworkID(uint16_t netid) const;
-		void setChannel(uint16_t channel) const;
-		void setZigbeePro(bool enabled) const;
+		void setNetworkID(uint16_t netid) const; //!< Set the PAN ID.
+		void setChannel(uint16_t channel) const; //!< Set the channel.
+		void setZigbeePro(bool enabled) const; //!< Enable/disable ZigBee PRO.
 
-		void enableEncryption(bool enabled) const;
+		void enableEncryption(bool enabled) const; //!< Enable/disable encryption.
+
+		/**
+		 * @brief Configure encryption.
+		 * @param opts Encryption options.
+		 */
 		void setEncryptionOptions(XBee::EncryptionOptions opts) const;
+
+		/**
+		 * @brief Set the link key.
+		 * @param key Key to set.
+		 * @note This key is used to encrypt the network key for joining devices.
+		 */
 		void setLinkKey(const ByteBuffer& key) const;
+
+		/**
+		 * @brief Set the network key.
+		 * @param key Pre-shared network key.
+		 */
 		void setNetworkKey(const ByteBuffer& key) const;
 
-		void setSleepMode(XBee::SleepMode mode) const;
-		uint16_t getParentAddress() const;
-		uint8_t getMaxPayloadSize() const;
-		void setMaxHops(uint8_t max) const;
-		void setNodeIdentifier(const stl::String& id) const;
-		void enableCoordinator(bool enable) const;
+		/**
+		 * @brief Configure the sleep strategy.
+		 * @param mode Sleep mode to set.
+		 */
+		void setSleepMode(XBee::SleepMode mode) const; //!< Configure the sleep mode.
+		uint16_t getParentAddress() const; //!< Get the parent device address.
+		uint8_t getMaxPayloadSize() const; //!< Get the maximum payload size.
+		void setMaxHops(uint8_t max) const; //!< Set the maximum number of hops.
+		void setNodeIdentifier(const stl::String& id) const; //!< Set the node ID.
+		void enableCoordinator(bool enable) const; //!< Enable/disable coordinator mode.
 
-		void writeToFlash() const;
+		void writeToFlash() const; //!< Write settings to flash.
 
 	protected:
-		void run() override;
-		void init();
+		void run() override; //!< Run the async driver.
+		void init(); //!< Start the driver.
 
 	private:
 		mutable Lock _lock;
--- include/lwiot/network/xbee/xbee.h
+++ include/lwiot/network/xbee/xbee.h
@@ -18,18 +18,26 @@
 #include <lwiot/network/xbee/xbeeaddress.h>
 #include <lwiot/network/xbee/xbeeresponse.h>
 #include <lwiot/network/xbee/xbeerequest.h>
-
 #include <lwiot/network/zigbeeaddress.h>
 
+#include <lwiot/stl/array.h>
 #include <lwiot/stl/referencewrapper.h>
+
 #include <lwiot/io/gpiopin.h>
 
 namespace lwiot
 {
 	class AsyncXbee;
 
+	/**
+	 * @brief XBee device driver.
+	 * @ingroup xb
+	 */
 	class XBee {
 	public:
+		/**
+		 * @brief Sleep mode.
+		 */
 		enum SleepMode {
 			NoSleep = 0,
 			PinHibernate = 1,
@@ -37,59 +45,109 @@ namespace lwiot
 			CyclicSleepPinWakeUp = 5
 		};
 
+		/**
+		 * @brief Encryption options.
+		 */
 		enum EncryptionOptions {
 			None = 0,
 			TrustCenter = 2,
 			UseTrustCenter = 8
 		};
 
-		explicit XBee();
-		virtual ~XBee() = default;
-		explicit XBee(const XBee& xb);
-		explicit XBee(const XBee&& xb) noexcept ;
+		explicit XBee(); //!< Construct a new XBee device driver.
+		virtual ~XBee() = default; //!< Destroy a XBee device driver.
+		explicit XBee(const XBee& xb); //!< Construct a new XBee device driver.
+		explicit XBee(XBee&& xb) noexcept ; //!< Construct a new XBee device driver.
 
-		XBee& operator=(const XBee& xb);
-		XBee& operator=(const XBee&& xb) noexcept ;
+		XBee& operator=(const XBee& xb); //!< Copy assignment operator.
+		XBee& operator=(XBee&& xb) noexcept ; //!< Move assignment operator.
 
-		void readPacket();
+		void readPacket(); //!< Read a single packet.
+
+		/**
+		 * @brief Attempt to read a packet for \p timeout miliseconds.
+		 * @see readPacket
+		 * @param timeout Period to attempt to read a packet.
+		 * @return True or false based on whether a new packet was received.
+		 */
 		bool readPacket(int timeout);
-		void readPacketUntilAvailable();
+		void readPacketUntilAvailable(); //!< Block unil a new packet has been received.
+
+		/**
+		 * @brief Start the XBee device driver.
+		 * @param serial Serial connection.
+		 */
 		void begin(Stream &serial);
+
+		/**
+		 * @brief Get the XBee response.
+		 * @param response The XBee response.
+		 */
 		void getResponse(XBeeResponse &response);
+
+		/**
+		 * @brief Get the XBee response.
+		 * @return The XBee response.
+		 */
 		XBeeResponse& getResponse();
-		void send(XBeeRequest &request) const;
-		void send(ZigbeeAddress addr, const ByteBuffer& buffer) const;
-		void send(ZigbeeAddress addr, const ByteBuffer& buffer, uint16_t profile, uint16_t cluster) const;
-		uint8_t getNextFrameId();
+
+		void send(XBeeRequest &request) const; //!< Send data.
+		void send(ZigbeeAddress addr, const ByteBuffer& buffer) const; //!< Send data.
+		void send(ZigbeeAddress addr, const ByteBuffer& buffer, uint16_t profile, uint16_t cluster) const; //!< Send data.
+		uint8_t getNextFrameId(); //!< Generate a new frame ID.
+
+		/**
+		 * @brief Set the serial driver.
+		 * @param serial Serial stream.
+		 */
 		void setSerial(Stream &serial);
+
+		/**
+		 * @brief Set the sleep request pin.
+		 * @param pin Sleep request pin to set.
+		 */
 		void setSleepPin(const GpioPin& pin);
 
-		void apply();
-		uint64_t getHardwareAddress();
-		void setNetworkID(uint16_t netid);
-		void setChannel(uint16_t channel);
-		void setZigbeePro(bool enabled);
-		uint16_t getNetworkAddress();
-		uint16_t getParentAddress();
+		void apply(); //!< Apply new settings.
 
-		void enableEncryption(bool enabled);
+		uint64_t getHardwareAddress(); //!< Get the hardware address.
+		void setNetworkID(uint16_t netid); //!< Set the PAN ID.
+		void setChannel(uint16_t channel); //!< Set the channel.
+		void setZigbeePro(bool enabled); //!< Enable/disable ZigBee PRO.
+		uint16_t getNetworkAddress(); //!< Get the network address.
+		uint16_t getParentAddress(); //!< Get the parent device address.
+
+		void enableEncryption(bool enabled); //!< Enable/disable encryption.
+
+		/**
+		 * @brief Configure encryption.
+		 * @param opts Encryption options.
+		 */
 		void setEncryptionOptions(EncryptionOptions opts);
+
+		/**
+		 * @brief Set the link key.
+		 * @param key Key to set.
+		 * @note This key is used to encrypt the network key for joining devices.
+		 */
 		void setLinkKey(const ByteBuffer& key);
+
+		/**
+		 * @brief Set the network key.
+		 * @param key Pre-shared network key.
+		 */
 		void setNetworkKey(const ByteBuffer& key);
-		void setNodeIdentifier(const stl::String& id);
-		void setMaxHops(uint8_t max);
-		void enableCoordinator(bool enable);
-
-		void writeToFlash();
-
-		uint8_t getMaxPayloadSize() const;
-
-		void setSleepMode(SleepMode sleepmode);
-		void sleep();
-		void wakeUp();
+		void setNodeIdentifier(const stl::String& id); //!< Set the node ID.
+		void setMaxHops(uint8_t max); //!< Set the maximum number of hops.
+		void enableCoordinator(bool enable); //!< Enable/disable coordinator mode.
+		void writeToFlash(); //!< Write settings to flash.
+		uint8_t getMaxPayloadSize() const; //!< Get the maximum payload size.
+		void setSleepMode(SleepMode sleepmode); //!< Configure the sleep mode.
+		void sleep(); //!< Sleep the XBee device.
+		void wakeUp(); //!< Wake the XBee device.
 
 	protected:
-		virtual void copy(const XBee& rhs);
+		virtual void copy(const XBee& rhs); //!< Copy an XBee device.
 
 	private:
 		friend class AsyncXbee;
@@ -100,7 +158,8 @@ namespace lwiot
 		uint8_t b;
 		uint8_t _checksumTotal;
 		uint8_t _nextFrameId;
-		uint8_t _responseFrameData[MAX_FRAME_DATA_SIZE];
+//		uint8_t _responseFrameData[MAX_FRAME_DATA_SIZE];
+		stl::Array<uint8_t, MAX_FRAME_DATA_SIZE> _responseFrameData;
 		uint8_t _max_payload;
 
 		stl::ReferenceWrapper<Stream> _serial;
